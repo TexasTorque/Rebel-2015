f8649ac6fe172828bf220621322203156dcd5e56
Noviv <matthewwebb65@yahoo.com>
2015-07-24 12:07:08 -0500

    reomved encoders for testing drivetrain

--- src/org/texastorque/texastorque20155/feedback/Feedback.java
+++ src/org/texastorque/texastorque20155/feedback/Feedback.java
@@ -6,10 +6,10 @@ import org.texastorque.torquelib.component.TorqueEncoder;
 
 public class Feedback {
 
-    private TorqueEncoder elevatorEncoder;
-
-    private TorqueEncoder leftDriveEncoder;
-    private TorqueEncoder rightDriveEncoder;
+//    private TorqueEncoder elevatorEncoder;
+//
+//    private TorqueEncoder leftDriveEncoder;
+//    private TorqueEncoder rightDriveEncoder;
 
     private double elevatorPosition;
     private double elevatorVelocity;
@@ -24,10 +24,10 @@ public class Feedback {
     private double rightDriveAcceleration;
 
     private Feedback() {
-        elevatorEncoder = new TorqueEncoder(Ports.ELEVATOR_ENCODER_A, Ports.ELEVATOR_ENCODER_B, false, EncodingType.k4X);
-
-        leftDriveEncoder = new TorqueEncoder(Ports.LEFT_DRIVE_ENCODER_A, Ports.LEFT_DRIVE_ENCODER_B, false, EncodingType.k4X);
-        rightDriveEncoder = new TorqueEncoder(Ports.RIGHT_DRIVE_ENCODER_A, Ports.RIGHT_DRIVE_ENCODER_B, false, EncodingType.k4X);
+//        elevatorEncoder = new TorqueEncoder(Ports.ELEVATOR_ENCODER_A, Ports.ELEVATOR_ENCODER_B, false, EncodingType.k4X);
+//
+//        leftDriveEncoder = new TorqueEncoder(Ports.LEFT_DRIVE_ENCODER_A, Ports.LEFT_DRIVE_ENCODER_B, false, EncodingType.k4X);
+//        rightDriveEncoder = new TorqueEncoder(Ports.RIGHT_DRIVE_ENCODER_A, Ports.RIGHT_DRIVE_ENCODER_B, false, EncodingType.k4X);
     }
 
     public void update() {
