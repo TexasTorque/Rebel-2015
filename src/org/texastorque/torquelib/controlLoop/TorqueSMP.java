b4083eacba268a0d65f4097caccbc020f17013db
Noviv <matthewwebb65@yahoo.com>
2015-11-05 20:39:28 -0600

    Switched to relative time

--- src/org/texastorque/torquelib/controlLoop/TorqueSMP.java
+++ src/org/texastorque/torquelib/controlLoop/TorqueSMP.java
@@ -2,10 +2,18 @@ package org.texastorque.torquelib.controlLoop;
 
 import static java.lang.Math.*;
 
+/**
+ * Created by Texas Torque (Matthew Webb)
+ */
 public class TorqueSMP {
 
+    //inputs
     private double maxV, maxA;
 
+    //variables
+    private double currentTime;
+
+    //constants
     private double t1, t2, t3;
     private double c, k;
     private double sigma;
@@ -19,6 +27,7 @@ public class TorqueSMP {
     public TorqueSMP(double mV, double mA) {
         maxV = mV;
         maxA = mA;
+        currentTime = 0.0;
     }
 
     private double func(double x) {
@@ -39,22 +48,27 @@ public class TorqueSMP {
 
         t2 = sigma - t1;
 
+        if (t2 < 0) {
+
+        }
+
         t3 = t2 + t1;
     }
 
     /**
      * Get the velocity of the profile at a specific time.
      *
-     * @param time Time
+     * @param time Change in time
      * @return Velocity
      */
-    public double getVelocity(double time) {
-        if (time < t1) {
-            return func(time);
-        } else if (time < t2) {
+    public double getVelocity(double dt) {
+        currentTime += dt;
+        if (currentTime < t1) {
+            return func(currentTime);
+        } else if (currentTime < t2) {
             return maxV;
-        } else if (time < t3) {
-            return -func(time - t2) + maxV;
+        } else if (currentTime < t3) {
+            return -func(currentTime - t2) + maxV;
         }
         return 0.0;
     }
