eb445dcd98e0c507b80b66187b69c09c782b29d0
Noviv <matthewwebb65@yahoo.com>
2015-11-05 18:26:57 -0600

    Fixed sigma and calculation of deceleration

--- src/org/texastorque/torquelib/controlLoop/TorqueSMP.java
+++ src/org/texastorque/torquelib/controlLoop/TorqueSMP.java
@@ -8,7 +8,8 @@ public class TorqueSMP {
 
     private double t1, t2, t3;
     private double c, k;
-
+    private double sigma;
+    
     public TorqueSMP(double mV, double mA) {
         maxV = mV;
         maxA = mA;
@@ -18,16 +19,14 @@ public class TorqueSMP {
         return (sin(k * x - PI / 2.0) + 1.0) / c;
     }
     
-    private double sigma(double x) {
-        return (-(t1 * maxV) / (2.0 * PI)) * cos((PI / t1) - (PI / 2)) + (1.0 / c) * x;
-    }
-
     public void generate(double distance) {
         t1 = PI * maxV / (2.0 * maxA);
         c = 2.0 / maxV;
         k = PI / t1;
         
-        t2 = sigma(distance) - t1;
+        sigma = (-(t1 * maxV) / (2.0 * PI)) * cos((PI / t1) - (PI / 2)) + (1.0 / c) * t1;
+        
+        t2 = sigma - t1;
         
         t3 = t2 + t1;
     }
@@ -38,7 +37,7 @@ public class TorqueSMP {
         } else if (time < t2) {
             return maxV;
         } else if (time < t3) {
-            return -func(time) + maxV;
+            return -func(time - t2) + maxV;
         }
         return 0.0;
     }
