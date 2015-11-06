df05ccffd2702366032476a1cf00f57cdb426df6
Noviv <matthewwebb65@yahoo.com>
2015-11-05 18:29:48 -0600

    Commented and fixed formatting

--- src/org/texastorque/torquelib/controlLoop/TorqueSMP.java
+++ src/org/texastorque/torquelib/controlLoop/TorqueSMP.java
@@ -9,28 +9,45 @@ public class TorqueSMP {
     private double t1, t2, t3;
     private double c, k;
     private double sigma;
-    
+
+    /**
+     * Create a new spline/sinusoidal motion profile. Units do not matter as long as they are constant.
+     *
+     * @param mV Maximum velocity
+     * @param mA Maximum acceleration
+     */
     public TorqueSMP(double mV, double mA) {
         maxV = mV;
         maxA = mA;
     }
-    
+
     private double func(double x) {
         return (sin(k * x - PI / 2.0) + 1.0) / c;
     }
-    
+
+    /**
+     * Generate the profile based a given distance.
+     *
+     * @param distance Distance
+     */
     public void generate(double distance) {
         t1 = PI * maxV / (2.0 * maxA);
         c = 2.0 / maxV;
         k = PI / t1;
-        
-        sigma = (-(t1 * maxV) / (2.0 * PI)) * cos((PI / t1) - (PI / 2)) + (1.0 / c) * t1;
-        
+
+        sigma = -((t1 * maxV) / (2.0 * PI)) * cos((PI / t1) - (PI / 2.0)) + (1.0 / c) * t1;
+
         t2 = sigma - t1;
-        
+
         t3 = t2 + t1;
     }
 
+    /**
+     * Get the velocity of the profile at a specific time.
+     *
+     * @param time Time
+     * @return Velocity
+     */
     public double getVelocity(double time) {
         if (time < t1) {
             return func(time);
@@ -41,8 +58,13 @@ public class TorqueSMP {
         }
         return 0.0;
     }
-    
-    public double getT3() {
+
+    /**
+     * Get the time the profile will take.
+     *
+     * @return Time
+     */
+    public double getMaxTime() {
         return t3;
     }
 }
